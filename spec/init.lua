21b91769a44c871071d9037a1ed8a7f82837e29b
SHBorg <al@shebang.org>
2024-09-15 01:37:51 +0200

    refactor(ulf.core): WIP

--- spec/init.lua
+++ spec/init.lua
@@ -1,13 +1,1 @@
-package.path = package.path .. ";" .. "/Users/al/config/nvim/lua/?.lua;" .. "/Users/al/config/nvim/lua/?/init.lua"
-package.path = package.path .. ";" .. os.getenv("PWD") .. "/../?.lua;" .. os.getenv("PWD") .. "/../?/init.lua"
-local Busted = require("corevim.core.busted")
-Busted.project_init()
 
--- local TestSuite = {}
--- _G.TestSuite = TestSuite
--- _G.inspect = require("spec.helpers.inspect")
---
--- function _G.P(v)
--- 	print("============ test debug ===========")
--- 	print(_G.inspect(v))
--- end
